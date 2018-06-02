c06feecc149ffb6de0371eb551386c68d3c9f8f8
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-06-02 14:54:50 -0300

    Convertendo projeto para maven project

--- src/Serial/SerialRxTx.java
+++ src/Serial/SerialRxTx.java
@@ -5,14 +5,25 @@
  */
 package Serial;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Enumeration;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import gnu.io.CommPortIdentifier;
 import gnu.io.SerialPort;
 import gnu.io.SerialPortEvent;
 import gnu.io.SerialPortEventListener;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.util.Enumeration;
 
 /**
  *
@@ -20,132 +31,158 @@ import java.util.Enumeration;
  */
 public class SerialRxTx implements SerialPortEventListener {
 
-    SerialPort serialPort = null;
+	SerialPort serialPort = null;
 
-    private Protocolo protocolo = new Protocolo();//objeto de gestão de protocolo
-    private String appName; //nome da aplicação
+	private Protocolo protocolo = new Protocolo();// objeto de gestão de protocolo
+	private String appName; // nome da aplicação
 
-    private BufferedReader input; //objeto para leitura na serial
-    private OutputStream output; //objeto para escrita na serial
+	private BufferedReader input; // objeto para leitura na serial
+	private OutputStream output; // objeto para escrita na serial
 
-    private static final int TIME_OUT = 1000; //tempo de espera de comunicação
-    private static int DATA_RATE = 9600; //velocidade da porta serial 
+	private static final int TIME_OUT = 1000; // tempo de espera de comunicação
+	private static int DATA_RATE = 9600; // velocidade da porta serial
 
-    private String serialPortName = "COM3";
+	private String serialPortName = "COM3";
 
-    public boolean iniciaSerial() {
-        boolean status = false;
+	private static final String API_TEMPERATURA_URL = " https://330f6520.ngrok.io/temperaturas";
 
-        try {
-            //obtém portas seriais do sistema
-            CommPortIdentifier portId = null;
-            Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+	public boolean iniciaSerial() {
+		boolean status = false;
 
-            while (portId == null && portEnum.hasMoreElements()) {
-                CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+		try {
+			// obtém portas seriais do sistema
+			CommPortIdentifier portId = null;
+			Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
 
-                if (currPortId.getName().equals(serialPortName) || currPortId.getName().startsWith(serialPortName)) {
-                    serialPort = (SerialPort) currPortId.open(appName, TIME_OUT);
-                    portId = currPortId;
-                    System.out.println("Conectado em: " + currPortId.getName());
-                    break;
-                }
-            }
+			while (portId == null && portEnum.hasMoreElements()) {
+				CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
 
-            if (portId == null || serialPort == null) {
-                return false;
-            }
+				if (currPortId.getName().equals(serialPortName) || currPortId.getName().startsWith(serialPortName)) {
+					serialPort = (SerialPort) currPortId.open(appName, TIME_OUT);
+					portId = currPortId;
+					System.out.println("Conectado em: " + currPortId.getName());
+					break;
+				}
+			}
 
-            serialPort.setSerialPortParams(DATA_RATE,
-                    SerialPort.DATABITS_8,
-                    SerialPort.STOPBITS_1,
-                    SerialPort.PARITY_NONE);
-            serialPort.addEventListener(this);
-            serialPort.notifyOnDataAvailable(true);
-            status = true;
+			if (portId == null || serialPort == null) {
+				return false;
+			}
 
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+			serialPort.setSerialPortParams(DATA_RATE, SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
+					SerialPort.PARITY_NONE);
+			serialPort.addEventListener(this);
+			serialPort.notifyOnDataAvailable(true);
+			status = true;
 
-            }
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 
-        } catch (Exception e) {
+			}
 
-            e.printStackTrace();
-            status = false;
+		} catch (Exception e) {
 
-        }
+			e.printStackTrace();
+			status = false;
 
-        return status;
-    }
-    
-    //Método que envia dados pela serial.
-    public void sendData(String data) {
-        try {
-            output = serialPort.getOutputStream();
-            output.write(data.getBytes());
-        } catch (Exception e) {
+		}
 
-            System.err.println(e.toString());
-            //retorna um erro, caso ocorra!
-        }
-    }
+		return status;
+	}
 
-    //Método que fecha a porta serial
-    public synchronized void close() {
+	// Método que envia dados pela serial.
+	public void sendData(String data) {
+		try {
+			output = serialPort.getOutputStream();
+			output.write(data.getBytes());
+		} catch (Exception e) {
 
-        if (serialPort != null) {
-            serialPort.removeEventListener();
-            serialPort.close();
-        }
+			System.err.println(e.toString());
+			// retorna um erro, caso ocorra!
+		}
+	}
 
-    }
+	// Método que fecha a porta serial
+	public synchronized void close() {
 
-    @Override
-    public void serialEvent(SerialPortEvent spe) {
-        //Método para lidar com dados que chegam pela serial
+		if (serialPort != null) {
+			serialPort.removeEventListener();
+			serialPort.close();
+		}
 
-        try {
-            switch (spe.getEventType()) {
-                case SerialPortEvent.DATA_AVAILABLE:
-                    if (input == null) {
-                        input = new BufferedReader(
-                                new InputStreamReader(
-                                        serialPort.getInputStream()));
-                    }
+	}
 
-                    if (input.ready()) {
-                        protocolo.setLeituraComando(input.readLine());
-                        
-                        System.out.println("temperatura: " + Double.valueOf(protocolo.getLeituraComando()).doubleValue());
-                    }
-                    break;
-                default:
-                    break;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+	@Override
+	public void serialEvent(SerialPortEvent spe) {
+		// Método para lidar com dados que chegam pela serial
 
-        }
+		try {
+			switch (spe.getEventType()) {
+			case SerialPortEvent.DATA_AVAILABLE:
+				if (input == null) {
+					input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+				}
 
-    }
-    
-    public static int getDATA_RATE() {
-        return DATA_RATE;
-    }
+				if (input.ready()) {
+					protocolo.setLeituraComando(input.readLine());
 
-    public static void setDATA_RATE(int DATA_RATE) {
-        SerialRxTx.DATA_RATE = DATA_RATE;
-    }
+					this.send(Double.valueOf(protocolo.getLeituraComando()).doubleValue());
+					//System.out.println("temperatura: " + Double.valueOf(protocolo.getLeituraComando()).doubleValue());
+				}
+				break;
+			default:
+				break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 
-    public String getSerialPortName() {
-        return serialPortName;
-    }
+		}
 
-    public void setSerialPortName(String serialPortName) {
-        this.serialPortName = serialPortName;
-    }
+	}
+
+	public void send(double temperatura) throws JSONException {
+		// ATEN��O,Isso � pra coloca logo abaixo a defini��o da classe, como qualquer
+		// outro atributo de classe
+		// Esse atributo aqui -> private static final String API_TEMPERATURA_URL = "
+		// https://330f6520.ngrok.io/temperaturas";
+
+		HttpClient client = HttpClientBuilder.create().build();
+
+		HttpPost post = new HttpPost(API_TEMPERATURA_URL);
+		post.setHeader("Content-type", "application/json");
+
+		JSONObject message = new JSONObject();
+		message.put("temperatura", temperatura);
+
+		post.setEntity(new StringEntity(message.toString(), "UTF-8"));
+		HttpResponse response = null;
+		try {
+			response = client.execute(post);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static int getDATA_RATE() {
+		return DATA_RATE;
+	}
+
+	public static void setDATA_RATE(int DATA_RATE) {
+		SerialRxTx.DATA_RATE = DATA_RATE;
+	}
+
+	public String getSerialPortName() {
+		return serialPortName;
+	}
+
+	public void setSerialPortName(String serialPortName) {
+		this.serialPortName = serialPortName;
+	}
 
 }
